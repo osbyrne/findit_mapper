fda5f39b1c34ea681ea2b70d5277c5207135e745
oscar <68084397+osbyrne@users.noreply.github.com>
2023-04-27 02:33:36 +0200

    helll no

---
+++ lib/item.dart
@@ -0,0 +1,9 @@
+class Item {
+  String? nom = '';
+  String? description = '';
+  String? reference = '';
+  String? indiceDeConsomation = '';
+  String? imageLink = '';
+  String? twoDLink = '';
+  String? threeDLink = '';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,132 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
+import 'item.dart';
 
-final _formKey = GlobalKey<FormState>();
+void main() => runApp(const MainPage());
 
-class FormWidget extends StatelessWidget {
-  const FormWidget({Key? key}) : super(key: key);
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'main page',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: const MyHomePage(title: ""),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const FormRoute(),
+            ),
+          );
+        },
+        tooltip: 'add new item DB',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class FormRoute extends StatelessWidget {
+  const FormRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Enter something';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Great!'),
-                    ),
-                  );
-                }
-              },
-              child: const Text('Validate'),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('new item'),
+      ),
+      body: const MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _item = Item();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(labelText: "nom de l'article"),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Entrez le nom de l'article";
+              }
+              if (value.length < 30) {
+                return "Entrez un nom plus court";
+              }
+              return null;
+            },
+            onSaved: (val) => setState(() => _item.nom = val),
+          ),
+          TextFormField(
+            decoration:
+                const InputDecoration(labelText: "description de l'article"),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Entrez la description de l'article";
+              }
+              if (value.length < 50) {
+                return "Entrez un nom plus court";
+              }
+              return null;
+            },
+            onSaved: (val) => setState(() => _item.description = val),
+          ),
+          //TODO need other fields
+
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Processing Data'),
+                  ),
+                );
+              }
+            },
+            child: const Text('Submit'),
+          ),
+        ],
       ),
     );
   }
